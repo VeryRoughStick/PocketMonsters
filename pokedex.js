9c31185e35aac01f7ba4382b825a948a1d949724
VeryRoughStick <ec2-user@ip-172-31-8-101.ec2.internal>
2023-02-21 17:55:41 +0000

    progress

--- pokedex.js
+++ pokedex.js
@@ -5,8 +5,17 @@ var pokedex= {}; //{1: {"name" : "bulbasaur", "img" : url, "type" ; ["grass,pois
 
 window.onload= async function(){
     getPokemon(1);
-}
-
+    for(let i=0; i<= pokemonCount; i++) {
+        await getPokemon(i);
+        
+        let pokemon = document.createElement("div");
+        pokemon.id = i;
+        pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+        pokemon.classList.add("pokemon-name");
+        document.getElementById("pokemon-list").append(pokemon);
+    }
+};
+ 
 async function getPokemon(num){
     let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
     
@@ -21,4 +30,7 @@ async function getPokemon(num){
     res = await fetch(pokemon["species"]["url"]);
     let pokemonDesc = await res.json();
     
+    pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"];
+    
+    pokedex[num] = {"name" : pokemonName, "img": pokemonImg, "types": pokemonType, "desc" : pokemonDesc};
 }
