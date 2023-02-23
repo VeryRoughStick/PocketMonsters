68a93f374b96665cb28b77120e5a289bb124c4dc
VeryRoughStick <ec2-user@ip-172-31-8-101.ec2.internal>
2023-02-23 17:52:16 +0000

    Starter 1

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 import fetch from 'node-fetch';
 
+var Starter; 
+
 function fetchKantoPokemon(){
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
         .then(response => response.json())
         .then(allpokemon => console.log(allpokemon));
 }
 
-// function giveStarter(){
-//     1 4 7
+function giveStarter(){
+    let StartVal = Math.floor(Math.random() * 3);
     
-// }
+    if(StartVal == 1){
+        Starter = 1;
+    }
+    else if(StartVal ==2){
+        Starter = 4;
+    }
+    else if(StartVal ==3){
+        Starter = 7;
+    }
+}
 
